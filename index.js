48c96f22e1e0a628a215bf3717eebfa7279f7be2
Anthony Hang <hang.anthony@gmail.com>
2022-11-21 21:03:10 -0500

    initial commit completed pair programming

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+
+/**
+Homework
+Here are some ideas to improve the application:
+link: https://socket.io/get-started/chat
+//*Broadcast a message to connected users when someone connects or disconnects.
+//*Add support for nicknames.
+//*Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
+//*Add “{user} is typing” functionality.
+//*Show who’s online.
+//*Add private messaging.
+//!Share your improvements!
+*/
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
+io.emit('some event', {
+  someProperty: 'some value',
+  otherProperty: 'other value'
+});
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
