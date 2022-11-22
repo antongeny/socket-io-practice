c622356c684db900347e5755d474607c94c24ffa
Anthony Hang <hang.anthony@gmail.com>
2022-11-21 21:36:28 -0500

    added some more things, but most new code added is inactive.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const io = new Server(server);
 Homework
 Here are some ideas to improve the application:
 link: https://socket.io/get-started/chat
+link: adding react hooks https://socket.io/how-to/use-with-react-hooks
 //*Broadcast a message to connected users when someone connects or disconnects.
 //*Add support for nicknames.
 //*Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
@@ -36,10 +37,7 @@ io.on('connection', (socket) => {
 //   });
 // });
 
-io.emit('some event', {
-  someProperty: 'some value',
-  otherProperty: 'other value'
-});
+
 
 
 
