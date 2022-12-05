e9df30afd6e2feb721894555df68e134789cac8a
Anthony Hang <hang.anthony@gmail.com>
2022-12-05 17:30:11 -0500

    testtest

--- components/App.js
+++ components/App.js
@@ -2,7 +2,7 @@
 // import io from 'socket.io-client';
 
 // const socket = io();
-//
+
 // function App() {
 //   const [isConnected, setIsConnected] = useState(socket.connected);
 //   const [lastPong, setLastPong] = useState(null);
